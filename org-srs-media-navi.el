9f27908bc1ff7803a0b6f18ea8f445ad9b7d5f72
Bohong Huang <bohonghuang@qq.com>
2026-06-22 01:15:59 +0800

    Add gamepad support for Android

--- org-srs-media-navi.el
+++ org-srs-media-navi.el
@@ -58,15 +58,27 @@
         (error "No child item")))))
 
 ;;;###autoload
+(defun org-srs-media-navi-toggle-review ()
+  (interactive)
+  (if (org-srs-reviewing-p)
+      (org-srs-review-quit)
+    (org-srs-review-start)))
+
+;;;###autoload
 (define-minor-mode org-srs-media-navi-mode
   "Minor mode for navigating org items during media review.
 \\{org-srs-media-navi-mode-map}"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "<up>") #'org-previous-item)
-            (define-key map (kbd "<down>") #'org-next-item)
-            (define-key map (kbd "<left>") #'org-srs-media-navi-parent-item)
-            (define-key map (kbd "<right>") #'org-srs-media-navi-first-child-item)
-            (define-key map (kbd "<SPC>") #'org-ctrl-c-ctrl-c)
+            (define-key map (kbd "<KEYCODE_DPAD_UP>") #'org-previous-item)
+            (define-key map (kbd "<KEYCODE_DPAD_DOWN>") #'org-next-item)
+            (define-key map (kbd "<KEYCODE_DPAD_LEFT>") #'org-srs-media-navi-parent-item)
+            (define-key map (kbd "<KEYCODE_DPAD_RIGHT>") #'org-srs-media-navi-first-child-item)
+            (define-key map (kbd "<KEYCODE_BUTTON_A>") #'org-srs-review-rate-good)
+            (define-key map (kbd "<KEYCODE_BUTTON_B>") #'org-srs-review-rate-again)
+            (define-key map (kbd "<KEYCODE_BUTTON_X>") #'org-srs-review-rate-easy)
+            (define-key map (kbd "<KEYCODE_BUTTON_Y>") #'org-srs-review-rate-hard)
+            (define-key map (kbd "<KEYCODE_BUTTON_START>") #'org-srs-media-navi-toggle-review)
+            (define-key map (kbd "<KEYCODE_BUTTON_SELECT>") #'org-srs-media-explain-this-entry)
             map))
 
 (defun org-srs-media-navi-setup (type &rest _args)
